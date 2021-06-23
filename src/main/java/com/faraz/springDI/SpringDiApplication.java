2b859a40b64f05268428eab20f7f78dfa83853ef
Faraz Noor <faraznoor75@gmail.com>
2021-06-23 22:24:46 +0530

    Understand application context

--- src/main/java/com/faraz/springDI/SpringDiApplication.java
+++ src/main/java/com/faraz/springDI/SpringDiApplication.java
@@ -1,13 +1,18 @@
 package com.faraz.springDI;
 
+import com.faraz.springDI.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringDiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		System.out.println(myController.sayHello());
 	}
 
 }
---
+++ src/main/java/com/faraz/springDI/controllers/MyController.java
@@ -0,0 +1,10 @@
+package com.faraz.springDI.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        return "Hello World";
+    }
+}
